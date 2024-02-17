0062c9abc92839a577498142d93e0ff008b2fcd9
Mateo Vasco <macv9620@utp.edu.co>
2024-02-16 19:57:02 -0500

    Se actualiza UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import { FocusTrap } from '@mui/base/FocusTrap';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import Button from '@mui/joy/Button';
+import Stack from '@mui/joy/Stack';
 
-function App() {
-  const [count, setCount] = useState(0)
+import CreateRoundedIcon from '@mui/icons-material/CreateRounded';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import FolderRoundedIcon from '@mui/icons-material/FolderRounded';
+
+import Layout from './components/Layout';
+import Navigation from './components/Navigation';
+import Mails from './components/Mails';
+import EmailContent from './components/EmailContent';
+import WriteEmail from './components/WriteEmail';
+import Header from './components/Header';
+
+export default function App() {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      {drawerOpen && (
+        <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
+          <Navigation />
+        </Layout.SideDrawer>
+      )}
+      <Stack
+        id="tab-bar"
+        direction="row"
+        justifyContent="space-around"
+        spacing={1}
+        sx={{
+          display: { xs: 'flex', sm: 'none' },
+          zIndex: '999',
+          bottom: 0,
+          position: 'fixed',
+          width: '100dvw',
+          py: 2,
+          backgroundColor: 'background.body',
+          borderTop: '1px solid',
+          borderColor: 'divider',
+        }}
+      >
+        <Button
+          variant="plain"
+          color="neutral"
+          aria-pressed="true"
+          component="a"
+          size="sm"
+          startDecorator={<EmailRoundedIcon />}
+          sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+        >
+          Email
+        </Button>
+        <Button
+          disabled
+          variant="plain"
+          color="neutral"
+          component="a"
+          size="sm"
+          startDecorator={<PeopleAltRoundedIcon />}
+          sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+        >
+          Team
+        </Button>
+        <Button
+          disabled
+          variant="plain"
+          color="neutral"
+          component="a"
+          size="sm"
+          startDecorator={<FolderRoundedIcon />}
+          sx={{ flexDirection: 'column', '--Button-gap': 0 }}
+        >
+          Files
+        </Button>
+      </Stack>
+      <Layout.Root
+        sx={{
+          ...(drawerOpen && {
+            height: '100vh',
+            overflow: 'hidden',
+          }),
+        }}
+      >
+        <Layout.Header>
+          <Header />
+        </Layout.Header>
+        <Layout.SideNav>
+          <Navigation />
+        </Layout.SideNav>
+        <Layout.SidePane>
+          <Box
+            sx={{
+              p: 2,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box sx={{ alignItems: 'center', gap: 1 }}>
+              <Typography level="title-lg" textColor="text.secondary" component="h1">
+                My inbox
+              </Typography>
+              <Typography level="title-sm" textColor="text.tertiary">
+                5 emails
+              </Typography>
+            </Box>
+            <Button
+              size="sm"
+              startDecorator={<CreateRoundedIcon />}
+              onClick={() => setOpen(true)}
+              sx={{ ml: 'auto' }}
+            >
+              Compose email
+            </Button>
+            <FocusTrap open={open} disableAutoFocus disableEnforceFocus>
+              <WriteEmail open={open} onClose={() => setOpen(false)} />
+            </FocusTrap>
+          </Box>
+          <Mails />
+        </Layout.SidePane>
+        <Layout.Main>
+          <EmailContent />
+        </Layout.Main>
+      </Layout.Root>
+    </CssVarsProvider>
+  );
 }
-
-export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,223 @@
+import * as React from 'react';
+import { useColorScheme } from '@mui/joy/styles';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import IconButton from '@mui/joy/IconButton';
+import Stack from '@mui/joy/Stack';
+import Avatar from '@mui/joy/Avatar';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Tooltip from '@mui/joy/Tooltip';
+import Dropdown from '@mui/joy/Dropdown';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+import ListDivider from '@mui/joy/ListDivider';
+import Drawer from '@mui/joy/Drawer';
+import ModalClose from '@mui/joy/ModalClose';
+import DialogTitle from '@mui/joy/DialogTitle';
+
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import BookRoundedIcon from '@mui/icons-material/BookRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
+import Navigation from './Navigation';
+
+function ColorSchemeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="outlined" color="primary" />;
+  }
+  return (
+    <Tooltip title="Change theme" variant="outlined">
+      <IconButton
+        id="toggle-mode"
+        size="sm"
+        variant="plain"
+        color="neutral"
+        sx={{ alignSelf: 'center' }}
+        onClick={() => {
+          if (mode === 'light') {
+            setMode('dark');
+          } else {
+            setMode('light');
+          }
+        }}
+      >
+        {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+export default function Header() {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexGrow: 1,
+        justifyContent: 'space-between',
+      }}
+    >
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={1}
+        sx={{ display: { xs: 'none', sm: 'flex' } }}
+      >
+        <Button
+          variant="plain"
+          color="neutral"
+          aria-pressed="true"
+          component="a"
+          size="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Email
+        </Button>
+        <Button
+          disabled
+          variant="plain"
+          color="neutral"
+          component="a"
+          size="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Team
+        </Button>
+        <Button
+          disabled
+          variant="plain"
+          color="neutral"
+          component="a"
+          size="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Files
+        </Button>
+      </Stack>
+      <Box sx={{ display: { xs: 'inline-flex', sm: 'none' } }}>
+        <IconButton variant="plain" color="neutral" onClick={() => setOpen(true)}>
+          <MenuRoundedIcon />
+        </IconButton>
+        <Drawer
+          sx={{ display: { xs: 'inline-flex', sm: 'none' } }}
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <ModalClose />
+          <DialogTitle>Acme Co.</DialogTitle>
+          <Box sx={{ px: 1 }}>
+            <Navigation />
+          </Box>
+        </Drawer>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 1.5,
+          alignItems: 'center',
+        }}
+      >
+        <Input
+          size="sm"
+          variant="outlined"
+          placeholder="Search anything…"
+          startDecorator={<SearchRoundedIcon color="primary" />}
+          sx={{
+            alignSelf: 'center',
+            display: {
+              xs: 'none',
+              sm: 'flex',
+            },
+          }}
+        />
+        <IconButton
+          size="sm"
+          variant="outlined"
+          color="neutral"
+          sx={{ display: { xs: 'inline-flex', sm: 'none' }, alignSelf: 'center' }}
+        >
+          <SearchRoundedIcon />
+        </IconButton>
+
+        <ColorSchemeToggle />
+        <Dropdown>
+          <MenuButton
+            variant="plain"
+            size="sm"
+            sx={{ maxWidth: '32px', maxHeight: '32px', borderRadius: '9999999px' }}
+          >
+            <Avatar
+              src="https://i.pravatar.cc/40?img=2"
+              srcSet="https://i.pravatar.cc/80?img=2"
+              sx={{ maxWidth: '32px', maxHeight: '32px' }}
+            />
+          </MenuButton>
+          <Menu
+            placement="bottom-end"
+            size="sm"
+            sx={{
+              zIndex: '99999',
+              p: 1,
+              gap: 1,
+              '--ListItem-radius': 'var(--joy-radius-sm)',
+            }}
+          >
+            <MenuItem>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar
+                  src="https://i.pravatar.cc/40?img=2"
+                  srcSet="https://i.pravatar.cc/80?img=2"
+                  sx={{ borderRadius: '50%' }}
+                />
+                <Box sx={{ ml: 1.5 }}>
+                  <Typography level="title-sm" textColor="text.primary">
+                    Rick Sanchez
+                  </Typography>
+                  <Typography level="body-xs" textColor="text.tertiary">
+                    rick@email.com
+                  </Typography>
+                </Box>
+              </Box>
+            </MenuItem>
+            <ListDivider />
+            <MenuItem disabled>
+              <HelpRoundedIcon />
+              Help
+            </MenuItem>
+            <MenuItem disabled>
+              <SettingsRoundedIcon />
+              Settings
+            </MenuItem>
+            <ListDivider />
+            <MenuItem>
+              <LogoutRoundedIcon />
+              Log out
+            </MenuItem>
+          </Menu>
+        </Dropdown>
+      </Box>
+    </Box>
+  );
+}
