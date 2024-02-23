6700a841edc6acfd118b62db5f3bbdc6a2134ba3
Mateo Vasco <macv9620@utp.edu.co>
2024-02-22 23:12:50 -0500

    Added version tag to test CI/CD

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -232,7 +232,10 @@ export default function Login() {
             </Box>
             <Box component="footer" sx={{ py: 3 }}>
               <Typography level="body-xs" textAlign="center">
-                © Ver Tech+ Mail {new Date().getFullYear()}
+                © Ver Tech+ Mail {new Date().getFullYear()} v.1.0.0
+              </Typography>
+              <Typography level="body-xs" textAlign="center">
+                v.1.0.0
               </Typography>
             </Box>
           </Box>
