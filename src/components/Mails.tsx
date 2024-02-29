2be0d18b54710d3d0310de558d74b020501e7813
Mateo Vasco <macv9620@utp.edu.co>
2024-02-28 23:44:20 -0500

    Implemented add categories to messages

--- src/components/Mails.tsx
+++ src/components/Mails.tsx
@@ -1,87 +1,103 @@
-import * as React from 'react';
-import Box from '@mui/joy/Box';
-import Typography from '@mui/joy/Typography';
-import Avatar from '@mui/joy/Avatar';
-import List from '@mui/joy/List';
-import ListDivider from '@mui/joy/ListDivider';
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton, { listItemButtonClasses } from '@mui/joy/ListItemButton';
-import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+import Avatar from "@mui/joy/Avatar";
+import List from "@mui/joy/List";
+import ListDivider from "@mui/joy/ListDivider";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton, { listItemButtonClasses } from "@mui/joy/ListItemButton";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
 
 type Props = {
-  messagesInfo: IMessageInfo[] | null,
-  setSelectedMessage: React.Dispatch<React.SetStateAction<IMessageInfo | null>>
-}
-export default function EmailList({messagesInfo, setSelectedMessage} :Props): JSX.Element {
-  const[selectedIndex, setSelectedIndex] = React.useState<number | null>(null)
-  const colors: string[] = ['success.400', 'warning.400', 'warning.400', 'danger.500', 'primary.500', 'success.400', 'success.400', 'warning.400', 'warning.400', 'danger.500', 'primary.500', 'success.400', 'success.400', 'warning.400', 'warning.400', 'danger.500', 'primary.500', 'success.400', 'success.400', 'warning.400', 'warning.400', 'danger.500', 'primary.500', 'success.400', 'success.400', 'warning.400', 'warning.400', 'danger.500', 'primary.500', 'success.400', 'success.400', 'warning.400', 'warning.400', 'danger.500', 'primary.500', 'success.400']
+  messagesInfo: IMessageInfo[] | null;
+  setSelectedMessage: React.Dispatch<React.SetStateAction<IMessageInfo | null>>;
+  selectedItem: string;
+};
+export default function EmailList({
+  messagesInfo,
+  setSelectedMessage,
+  selectedItem,
+}: Props): JSX.Element {
+  const [selectedIndex, setSelectedIndex] = React.useState<number | null>(null);
 
   return (
-    <Box
-      sx={{overflow: 'auto'}}
-    >
-    <List
-      sx={{
-        [`& .${listItemButtonClasses.root}.${listItemButtonClasses.selected}`]: {
-          borderLeft: '2px solid',
-          borderLeftColor: 'var(--joy-palette-primary-outlinedBorder)',
-        }
-      }}
-    >
-      {messagesInfo?.map((item, index) => (
-        <React.Fragment key={index}>
-          <ListItem>
-            <ListItemButton
-              {...(index === selectedIndex && {
-                selected: true,
-                color: 'neutral',
-              })}
-              sx={{ p: 2 }}
-              onClick={()=> {
-                setSelectedMessage(item)
-                setSelectedIndex(index)
-                }
-              }
-            >
-              <ListItemDecorator sx={{ alignSelf: 'flex-start' }}>
-                <Avatar alt="" src={'https://placehold.co/155x232/f6f8fa/black?text=' + item.from_user_name[0].toUpperCase()} />
-              </ListItemDecorator>
-              <Box sx={{ pl: 2, width: '100%' }}>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    mb: 0.5,
-                  }}
-                >
-                  <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-                    <Typography level="body-xs">{item.from_user_name}</Typography>
+    <Box sx={{ overflow: "auto" }}>
+      <List
+        sx={{
+          [`& .${listItemButtonClasses.root}.${listItemButtonClasses.selected}`]:
+            {
+              borderLeft: "2px solid",
+              borderLeftColor: "var(--joy-palette-primary-outlinedBorder)",
+            },
+        }}
+      >
+        {messagesInfo?.map((item, index) => (
+          <React.Fragment key={index}>
+            <ListItem>
+              <ListItemButton
+                {...(index === selectedIndex && {
+                  selected: true,
+                  color: "neutral",
+                })}
+                sx={{ p: 2 }}
+                onClick={() => {
+                  setSelectedMessage(item);
+                  setSelectedIndex(index);
+                }}
+              >
+                <ListItemDecorator sx={{ alignSelf: "flex-start" }}>
+                  <Avatar
+                    alt=""
+                    src={
+                      "https://placehold.co/155x232/842520/white?text=" +
+                      (selectedItem === "inbox"
+                        ? item?.from_user_name[0].toUpperCase()
+                        : item?.to_user_name[0].toUpperCase())
+                    }
+                  />
+                </ListItemDecorator>
+                <Box sx={{ pl: 2, width: "100%" }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      mb: 0.5,
+                    }}
+                  >
                     <Box
-                      sx={{
-                        width: '8px',
-                        height: '8px',
-                        borderRadius: '99px',
-                        bgcolor: colors[index],
-                      }}
-                    />
+                      sx={{ display: "flex", alignItems: "center", gap: 0.5 }}
+                    >
+                      <Typography level="body-xs">
+                        {selectedItem == "inbox"
+                          ? item.from_user_name
+                          : item.to_user_name}
+                      </Typography>
+                      <Box
+                        sx={{
+                          width: "8px",
+                          height: "8px",
+                          borderRadius: "99px",
+                          bgcolor: item.category_id === 0 ? "" : item.color,
+                        }}
+                      />
+                    </Box>
+                    <Typography level="body-xs" textColor="text.tertiary">
+                      {item.created_at}
+                    </Typography>
                   </Box>
-                  <Typography level="body-xs" textColor="text.tertiary">
-                    {item.created_at}
-                  </Typography>
+                  <div>
+                    <Typography sx={{ mb: 0.5 }}>{item.subject}</Typography>
+                    <Typography level="body-sm">
+                      {item.body.slice(0, 60) + "..."}
+                    </Typography>
+                  </div>
                 </Box>
-                <div>
-                  <Typography sx={{ mb: 0.5 }}>
-                    {item.subject}
-                  </Typography>
-                  <Typography level="body-sm">{item.body.slice(0, 60) + '...'}</Typography>
-                </div>
-              </Box>
-            </ListItemButton>
-          </ListItem>
-          <ListDivider sx={{ m: 0 }} />
-        </React.Fragment>
-      ))}
-    </List>
+              </ListItemButton>
+            </ListItem>
+            <ListDivider sx={{ m: 0 }} />
+          </React.Fragment>
+        ))}
+      </List>
     </Box>
   );
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,10 +5,14 @@ interface IMessageInfo {
     from_user_name: string,
     from_user: string,
     to_user: string,
+    to_user_name: string,
     avatar?: string,
     created_at: string,
     subject: string,
     body: string,
+    color: string,
+    category_id: number,
+    category_name: string,
     color: string
 }
 
@@ -47,4 +51,9 @@ interface IUserPostCategory {
   email: string,
   category_name: string,
   color: string
+}
+
+interface IUpdateMessageCategory {
+  message_id: number | undefined,
+  category_id: number  | null
 }
